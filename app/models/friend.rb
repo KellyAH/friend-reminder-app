d4afbb265d445a26ab2585036e5d4a039fa418e3
Kelly Hong <k_ahong@yahoo.com>
2020-03-28 12:48:23 -0700

    save current code state

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,5 +1,9 @@
 class Friend < ApplicationRecord
-  has_many :events
+  has_many :events, dependent: :destroy
   validates_presence_of :first_name, :last_name, :email, :status
   enum status: { active: 'active', deactivated: 'deactivated', pending: 'pending' }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
