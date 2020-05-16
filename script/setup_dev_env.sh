0529f5ce0eb2d9481e2b5ca62edf2f99c1638fb6
Kelly Hong <k_ahong@yahoo.com>
2020-05-16 13:53:33 -0700

    edit script to create db in container

--- script/setup_dev_env.sh
+++ script/setup_dev_env.sh
@@ -11,10 +11,9 @@ docker build -t friend-reminder:latest .
 # use docker image to spin up container
 docker container run --name friend-reminder-app -p 3000:3000 friend-reminder:latest
 
-
 #docker container run --name friend-reminder-app -v $PWD:/friend_reminder_app -p 3000:3000 friend-reminder:latest
 
 # create empty database
-#docker exec -ti friend-reminder-app sh -c "rails db:migrate RAILS_ENV=development"
+docker exec -ti friend-reminder-app sh -c "rails db:migrate RAILS_ENV=development"
 
-# start rails server
+
