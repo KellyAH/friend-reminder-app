7367be35fed57322a1198dd41d19729c2dd58582
Kelly Hong <k_ahong@yahoo.com>
2020-06-28 22:43:43 -0700

    update text displayed on root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "Hiya"
+    render html: "Welcome to the Friend reminder app. Please go to http://localhost:3000/friends or http://localhost:3000/events"
   end
 
   def goodbye
