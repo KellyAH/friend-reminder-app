6bc2bb3641b75195aebbf1effd612219e7c63fe6
Kelly Hong <k_ahong@yahoo.com>
2020-07-12 23:12:48 -0700

    create spec tests for friend model

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,4 +1,6 @@
 class Friend < ApplicationRecord
+  scope :active_friends, -> { where(status: 'active') }
+  scope :pending_friends, -> { where(status: 'pending') }
   has_many :events, dependent: :destroy
   validates_presence_of :first_name, :last_name, :email, :status
   enum status: { active: 'active', deactivated: 'deactivated', pending: 'pending' }
