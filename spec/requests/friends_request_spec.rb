72241175dd2facca7d0900eaf4a86a6662dc2c7e
Kelly Hong <k_ahong@yahoo.com>
2020-07-25 16:03:51 -0700

    remove uneeded controller request tests

--- spec/requests/friends_request_spec.rb
+++ spec/requests/friends_request_spec.rb
@@ -2,18 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "Friends", type: :request do
   # TODO fix these
-  # context 'GET #index' do
-  #   it 'returns a success response' do
-  #     get :index
-  #     expect(response).to be_success
-  #   end
+  # https://stackoverflow.com/questions/49787383/rspec-controller-testing-http-test-host-login-error-on-index-spec-test
+
+  # before do
+  #   @request.host = "localhost:3000"
   # end
-  #
-  # context 'GET #show' do
-  #   it 'returns a success response' do
-  #     friend = Friend.create!(first_name: 'Jane', last_name: 'Doe', email: 'test@test.com')
-  #     get :show, params: { id: friend.to_param}
-  #     expect(response).to be_success
-  #   end
-  # end
+
+  context 'GET #friends' do
+    it 'returns a success response' do
+      get :friends
+      expect(response).to be_success
+    end
+  end
+
+  context 'GET #events' do
+    it 'returns a success response' do
+      get :events
+      expect(response).to be_success
+    end
+  end
+
+  context 'GET #events' do
+    it 'returns a success response' do
+      friend = Friend.create!(first_name: 'Jane', last_name: 'Doe', email: 'test@test.com')
+      get :show, params: { id: friend.to_param}
+      expect(response).to be_success
+    end
+  end
 end
